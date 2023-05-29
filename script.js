17edb9ae918cdd89b3a6c7e3f65ce7b7b757d62b
Sua <sgua0720@gmail.com>
2023-05-29 22:15:46 +0900

    커밋 메세지

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+
+const createTodo = function () {
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+  });
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+};
 
 function keyCodeCheck(event) {
   if (event.key === "Enter" && todoInput.value != "") {
-    const todoList = document.querySelector("#todo-list");
-    const newLi = document.createElement("li");
-    const newSpan = document.createElement("span");
-
-    newSpan.textContent = todoInput.value;
-    newLi.appendChild(newSpan);
-    todoList.appendChild(newLi);
-    todoInput.value = "";
+    createTodo();
   }
 }
-
 document.querySelector("#todo-input").addEventListener("keydown", keyCodeCheck);
+
+const deleteAll = function () {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
+  }
+};
+
+const saveItmesFn = function () {
+  const saveItmes = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItmes.push(todoObj);
+    saveItmes.push(todo);
+  }
+};
